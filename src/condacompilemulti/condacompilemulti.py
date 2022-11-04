a5541600b3f35c74a16cb69869efbc35350eac89
Neil Vaytet <neil.vaytet@esss.se>
2022-11-04 21:39:15 +0100

    proces multiple files

--- src/condacompilemulti/condacompilemulti.py
+++ src/condacompilemulti/condacompilemulti.py
@@ -1,29 +1,43 @@
-import argparse
+import glob
 import os
 import pathlib
 import subprocess
 import sys
 import uuid
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('-i', '--input-file')
-parser.add_argument('-o', '--output-file')
 
-if __name__ == '__main__':
+def parse_input_file(filename):
+    with open(filename, 'r') as f:
+        content = f.readlines()
+    out = []
+    for line in content:
+        if line.startswith('-r'):
+            out += parse_input_file(line.split()[-1])
+        else:
+            out.append(line.strip())
+    return out
 
-    args = parser.parse_args()
+
+def process_file(filename):
+
+    dependencies = parse_input_file(filename)
 
     env_name = uuid.uuid4().hex
+    out = subprocess.check_output(
+        ['mamba', 'create', '-c', 'conda-forge', '--dry-run', '-n', env_name] +
+        dependencies,
+        stderr=subprocess.STDOUT,
+        shell=sys.platform == "win32")
 
-    out = subprocess.check_output([
-        'mamba', 'create', '-c', 'conda-forge', '--dry-run', '--file',
-        args.input_file, '-n', env_name
-    ],
-                                  stderr=subprocess.STDOUT,
-                                  shell=sys.platform == "win32")
-
-    with open(args.output_file, 'w') as f:
+    with open(os.path.splitext(filename)[0] + '.txt', 'w') as f:
         for l in out.decode().split('\n'):
             if l.strip().startswith('+'):
                 to_write = l.lstrip(' +').split()[:2]
                 f.write(f'{to_write[0]}={to_write[1]}\n')
+
+
+if __name__ == '__main__':
+
+    for file in glob.glob('*.in'):
+        print('Locking dependencies in', file)
+        process_file(file)
